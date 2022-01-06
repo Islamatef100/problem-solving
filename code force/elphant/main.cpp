dd20fd8a7b11a748fb4b8b174021c192986e6b2f
islam atef nagy <78918678+Islamatef100@users.noreply.github.com>
2022-01-06 21:23:01 +0200

    Add files via upload

---
+++ code force/elphant/main.cpp
@@ -0,0 +1,22 @@
+//elphant
+ #include <iostream>
+ #include <string>
+ using namespace std;
+ int main()
+ {
+ int t=0;
+ cout<<"enter the position:";cin>>t;
+ if(t%5==0)
+ cout<<t/5<<endl;
+ else if(t%3==0)
+ cout<<t/3<<endl;
+ else if(t%4==0)
+ {
+ cout<<t/4<<endl;
+ }
+ else if(t%2==0)
+ cout<<t/2<<endl;
+ 
+ }
+ 
+ 
