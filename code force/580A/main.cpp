78098b12bd38ed0861a8c27353f6b7e72c58ed2c
islam atef nagy <78918678+Islamatef100@users.noreply.github.com>
2022-01-06 19:33:27 +0200

    Add files via upload

---
+++ code force/580A/main.cpp
@@ -0,0 +1,15 @@
+//580A - Kefa and First Steps
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int t[100000];
+    for(int i=0;i<n;i++)
+    {
+        cin>>t[i];
+    }
+    cout<<"3";
+}
