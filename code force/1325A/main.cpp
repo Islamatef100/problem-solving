d27c9c14a30d2c073660b8d4152f8a9e15b35158
islam atef nagy <78918678+Islamatef100@users.noreply.github.com>
2022-01-06 20:05:08 +0200

    Add files via upload

---
+++ code force/1325A/main.cpp
@@ -0,0 +1,18 @@
+//1325A - EhAb AnD gCd
+ #include<iostream>
+ using namespace std ;
+ void solve ()
+ {
+ int x ;
+ cin >> x ;
+ cout << 1 << ' ' << x-1 << '\n' ;
+ }
+ 
+ int main()
+ {
+ int t = 1 ;
+ cin>>t ;
+ while (t--)
+ solve () ;
+ return 0 ;
+ }
