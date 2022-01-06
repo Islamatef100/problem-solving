acc7e1d24115f4a81f12a9b3d4869bf07ba7cf48
islam atef nagy <78918678+Islamatef100@users.noreply.github.com>
2022-01-06 18:59:24 +0200

    Add files via upload

---
+++ code force/270A/main.cpp
@@ -0,0 +1,21 @@
+// 270A - Fancy Fence
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t, a;
+    cin >> t;
+    while (t--)
+    {
+        cin >> a;
+        if (360 % (180 - a) == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
