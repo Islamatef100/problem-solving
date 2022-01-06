fa566dda21f4553ede9b281d102128ee2e81570e
islam atef nagy <78918678+Islamatef100@users.noreply.github.com>
2022-01-06 20:59:07 +0200

    Add files via upload

---
+++ code force/1369A/main.cpp
@@ -0,0 +1,21 @@
+//1369A - FashionabLee
+#include <iostream>
+ 
+ using namespace std;
+ 
+ int main()
+ {
+ int n,t;
+ cin >> n;
+ while (n--)
+ {
+ cin >> t;
+ if (t%4==0)
+ {
+ cout<<"YES\n";
+ }
+ else
+ {cout<<"NO\n";
+ }
+ }
+ }
