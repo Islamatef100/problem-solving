b48c517549cfc4ff61f80b7fa2bfb1f052753e33
islam atef nagy <78918678+Islamatef100@users.noreply.github.com>
2022-01-06 18:35:13 +0200

    Add files via upload

---
+++ code force/A_Mahmoud_and_Ehab_and_the_even_odd_game/main.cpp
@@ -0,0 +1,13 @@
+//959A - Mahmoud and Ehab and the even-odd game 
+//  Created by Mac on 10/10/21.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(n%2==0)
+        cout<<"Mahmoud\n";
+    else
+        cout<<"Ehab\n";
+}
