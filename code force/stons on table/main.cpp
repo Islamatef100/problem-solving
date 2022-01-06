772b33de55b15078b57b4e65aea26d0ed60afecb
islam atef nagy <78918678+Islamatef100@users.noreply.github.com>
2022-01-06 18:42:05 +0200

    Add files via upload

---
+++ code force/stons on table/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string x="RRBBGG";
+    int n=6,k=0;
+    if(x.length()!=n)
+        cout<<"error==>n nor equal collersn\n";
+    for(int i=0;i<x.length();i++)
+    {
+        for(int j=i+1;j<x.length();j++)
+        {
+            if(x.at(i)==x.at(j))
+                ++k;
+        }
+    }
+    cout<<k<<endl;
+    
+   
+}
